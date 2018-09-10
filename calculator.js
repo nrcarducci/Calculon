e12ff7676b8675f90b6449a1becfe70f0a57a2a7
James Buxton <codingforcoffee@gmail.com>
2018-09-10 15:35:01 -0700

    okay this commit has all of the changes that I thought the last commit had.  the display of very large numbers has been accomodated with if statements that adjust the font size and word-wrap properties of displayBox.innerHTML which are attached to every button's event listener function

--- calculator.js
+++ calculator.js
@@ -14,39 +14,98 @@ let button7 = document.getElementById("calcBut7");
 let button8 = document.getElementById("calcBut8");
 let button9 = document.getElementById("calcBut9");
 let button0 = document.getElementById("calcBut0");
-let button00 = document.getElementById("calcBut00");
 
 /*-add event listeners to the number buttons-*/
 
 button1.addEventListener("click", function(){
    displayBox.innerHTML += 1;
+   if(displayBox.innerHTML.length >12){
+      displayBox.style.fontSize = "1.75em";
+   }
+   if(displayBox.innerHTML.length >20){
+      displayBox.style.wordWrap = "break-word";
+   }
 })
 button2.addEventListener("click", function(){
    displayBox.innerHTML += 2;
+   if(displayBox.innerHTML.length >12){
+      displayBox.style.fontSize = "1.75em";
+   }
+   if(displayBox.innerHTML.length >20){
+      displayBox.style.wordWrap = "break-word";
+   }
 })
 button3.addEventListener("click", function(){
    displayBox.innerHTML += 3;
+   if(displayBox.innerHTML.length >12){
+      displayBox.style.fontSize = "1.75em";
+   }
+   if(displayBox.innerHTML.length >20){
+      displayBox.style.wordWrap = "break-word";
+   }
 })
 button4.addEventListener("click", function(){
    displayBox.innerHTML += 4;
+   if(displayBox.innerHTML.length >12){
+      displayBox.style.fontSize = "1.75em";
+   }
+   if(displayBox.innerHTML.length >20){
+      displayBox.style.wordWrap = "break-word";
+   }
 })
 button5.addEventListener("click", function(){
    displayBox.innerHTML += 5;
+   if(displayBox.innerHTML.length >12){
+      displayBox.style.fontSize = "1.75em";
+   }
+   if(displayBox.innerHTML.length >20){
+      displayBox.style.wordWrap = "break-word";
+   }
 })
 button6.addEventListener("click", function(){
    displayBox.innerHTML += 6;
+   if(displayBox.innerHTML.length >12){
+      displayBox.style.fontSize = "1.75em";
+   }
+   if(displayBox.innerHTML.length >20){
+      displayBox.style.wordWrap = "break-word";
+   }
 })
 button7.addEventListener("click", function(){
    displayBox.innerHTML += 7;
+   if(displayBox.innerHTML.length >12){
+      displayBox.style.fontSize = "1.75em";
+   }
+   if(displayBox.innerHTML.length >20){
+      displayBox.style.wordWrap = "break-word";
+   }
 })
 button8.addEventListener("click", function(){
    displayBox.innerHTML += 8;
+   if(displayBox.innerHTML.length >12){
+      displayBox.style.fontSize = "1.75em";
+   }
+   if(displayBox.innerHTML.length >20){
+      displayBox.style.wordWrap = "break-word";
+   }
 })
 button9.addEventListener("click", function(){
    displayBox.innerHTML += 9;
+   if(displayBox.innerHTML.length >12){
+      displayBox.style.fontSize = "1.75em";
+   }
+   if(displayBox.innerHTML.length >20){
+      displayBox.style.wordWrap = "break-word";
+   }
 })
 button0.addEventListener("click", function(){
    displayBox.innerHTML += 0;
+   if(displayBox.innerHTML.length >12){
+      displayBox.style.fontSize = "1.75em";
+   }
+   if(displayBox.innerHTML.length >20){
+      displayBox.style.wordWrap = "break-word";
+   }
 })
 
 /*------declare the non-number buttons------*/
@@ -66,31 +125,86 @@ let buttonPlusMinus = document.getElementById("calcButPlus_Minus");
 
 buttonAC.addEventListener("click", function(){
    displayBox.innerHTML = "";
+   displayBox.style.fontSize = "3em";
 })
 buttonLP.addEventListener("click", function(){
    displayBox.innerHTML += "(";
+   if(displayBox.innerHTML.length >12){
+      displayBox.style.fontSize = "1.75em";
+   }
+   if(displayBox.innerHTML.length >20){
+      displayBox.style.wordWrap = "break-word";
+   }
 })
 buttonRP.addEventListener("click", function(){
    displayBox.innerHTML += ")";
+   if(displayBox.innerHTML.length >12){
+      displayBox.style.fontSize = "1.75em";
+   }
+   if(displayBox.innerHTML.length >20){
+      displayBox.style.wordWrap = "break-word";
+   }
 })
 buttonDiv.addEventListener("click", function(){
    displayBox.innerHTML += "/";
+   if(displayBox.innerHTML.length >12){
+      displayBox.style.fontSize = "1.75em";
+   }
+   if(displayBox.innerHTML.length >20){
+      displayBox.style.wordWrap = "break-word";
+   }
 })
 buttonMult.addEventListener("click", function(){
    displayBox.innerHTML += "*";
+   if(displayBox.innerHTML.length >12){
+      displayBox.style.fontSize = "1.75em";
+   }
+   if(displayBox.innerHTML.length >20){
+      displayBox.style.wordWrap = "break-word";
+   }
 })
 buttonAdd.addEventListener("click", function(){
    displayBox.innerHTML += "+";
+   if(displayBox.innerHTML.length >12){
+      displayBox.style.fontSize = "1.75em";
+   }
+   if(displayBox.innerHTML.length >20){
+      displayBox.style.wordWrap = "break-word";
+   }
 })
 buttonSub.addEventListener("click", function(){
    displayBox.innerHTML += "-";
+   if(displayBox.innerHTML.length >12){
+      displayBox.style.fontSize = "1.75em";
+   }
+   if(displayBox.innerHTML.length >20){
+      displayBox.style.wordWrap = "break-word";
+   }
 })
 buttonEq.addEventListener("click", function(){
    displayBox.innerHTML = eval(String(displayBox.innerHTML));
+   if(displayBox.innerHTML.length >12){
+      displayBox.style.fontSize = "1.75em";
+   }
+   if(displayBox.innerHTML.length >20){
+      displayBox.style.wordWrap = "break-word";
+   }
 })
 buttonDot.addEventListener("click", function(){
    displayBox.innerHTML += ".";
+   if(displayBox.innerHTML.length >12){
+      displayBox.style.fontSize = "1.75em";
+   }
+   if(displayBox.innerHTML.length >20){
+      displayBox.style.wordWrap = "break-word";
+   }
 })
 buttonPlusMinus.addEventListener("click", function(){
    displayBox.innerHTML = displayBox.innerHTML *(-1);
+   if(displayBox.innerHTML.length >12){
+      displayBox.style.fontSize = "1.75em";
+   }
+   if(displayBox.innerHTML.length >20){
+      displayBox.style.wordWrap = "break-word";
+   }
 })
